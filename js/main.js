1069df2cd464f959e466a2a4392d9772727dbdd1
ZohidovaRoziya <rzohidova8@gmail.com>
2022-01-13 22:46:56 +0500

    remove site unnecessary logo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 let overlay_El = document.getElementById("overlay");
 let aside_modal_card_El = document.getElementById("aside_modal_card")
 let modal_close_btn_El = document.getElementById("modal_close_btn")
-let aside_link_on_El = document.getElementById("site_aside_link_on")
-let aside_link_off_El = document.getElementById("site_aside_link_off")
+let aside_link_on_El = document.getElementById("aside_link_on")
+let aside_link_off_El = document.getElementById("aside_link_off")
 
 
 aside_link_on_El.addEventListener("click", ()=>{
-    
+    overlay_El.classList.remove("hidden")
+    aside_modal_card_El.classList.remove("hidden")
 })
 
 
